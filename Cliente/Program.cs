aa265ecb9a3375063474ad1db28e8bcc5726bb32
sixtos04@gmail.com <sixtos04@gmail.com>
2022-11-02 02:29:48 -0600

    Cliente funciona ok

--- Cliente/Program.cs
+++ Cliente/Program.cs
@@ -7,9 +7,10 @@ namespace Cliente
 {
     class Program
     {
-        public const int MAX_BYTES = 1024;
+        public const int MAX_BYTES = 10240;
         static void Main(string[] args)
         {
+            System.Console.Title = "Cliente";
             IPHostEntry host = Dns.GetHostEntry("localhost");
             IPAddress ip_addr = host.AddressList[0];
             IPEndPoint remote_ep = new IPEndPoint(ip_addr, 11200);
@@ -21,19 +22,21 @@ namespace Cliente
 
                 byte[] bytes_to_send, bytes_recvd = new byte[MAX_BYTES];
                 int num_bytes_recvd;
-                string text_to_send = "", text_recvd = "";
+                string query = "", text_recvd = "";
 
-                global::System.Console.WriteLine("Ingrese texto para enviar al Servidor: ");
-
-                while (text_to_send != "exit")
+                while(true)
                 {
-                    text_to_send = Console.ReadLine();
-                    bytes_to_send = Encoding.ASCII.GetBytes(text_to_send + "<EOF>");
+                    Console.Write("\n\n\nBuscar en Wikipedia: ");
+                    query = Console.ReadLine();
+
+                    if (query == "exit") break;
+
+                    bytes_to_send = Encoding.ASCII.GetBytes(query + "<EOF>");
                     sender.Send(bytes_to_send);
 
                     num_bytes_recvd = sender.Receive(bytes_recvd);
-                    text_recvd = Encoding.ASCII.GetString(bytes_recvd, 0, num_bytes_recvd);
-                    global::System.Console.WriteLine("Servidor: " + text_recvd);
+                    text_recvd = Encoding.UTF8.GetString(bytes_recvd, 0, num_bytes_recvd);
+                    global::System.Console.WriteLine("\n\n\nServidor: \n\n" + text_recvd);
                 }
                 sender.Shutdown(SocketShutdown.Both);
                 sender.Close();
